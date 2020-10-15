bd5a8afa0031a497b8e8c0a458a17039623583c1
wenhao <wenhaogoh1998@gmail.com>
2020-10-15 10:55:19 +0800

    Fix result display overrun

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -8,8 +8,6 @@ import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
-import seedu.address.logic.commands.main.DeleteBudgetCommand;
-import seedu.address.logic.commands.main.OpenBudgetCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.expenditure.Price;
 import seedu.address.model.tag.Tag;
