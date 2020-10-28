26828a45d4483ac51fd6c4c86b080cabda0a7789
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-10-28 18:36:48 +0800

    Add unit testing for state manager

--- src/main/java/seedu/address/state/budgetindex/BudgetIndexManager.java
+++ src/main/java/seedu/address/state/budgetindex/BudgetIndexManager.java
@@ -5,12 +5,18 @@ import java.util.Optional;
 public class BudgetIndexManager implements BudgetIndex {
     private int budgetIndex;
 
+    /**
+     * Constructor for BudgetIndexManager.
+     * @param budgetIndex the index of the current budget.
+     */
     public BudgetIndexManager(int budgetIndex) {
+        assert budgetIndex >= 0;
         this.budgetIndex = budgetIndex;
     }
 
     @Override
     public void setIndex(int index) {
+        assert index >= 0;
         this.budgetIndex = index;
     }
 
