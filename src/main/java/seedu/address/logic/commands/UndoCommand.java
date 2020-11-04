5e3be01dd4c685c81db417595f970fc0d40c9fcd
wenhao <wenhaogoh1998@gmail.com>
2020-11-04 19:00:52 +0800

    Add dummy command result

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -15,6 +15,6 @@ public class UndoCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        return new CommandResult(UNDO_MESSAGE_SUCCESS);
     }
 }
