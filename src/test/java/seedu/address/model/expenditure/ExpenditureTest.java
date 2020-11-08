2ae73ae548d5dde13e7d050388214a310139d75a
yu-ming-chen <chen.yuming.2015@gmail.com>
2020-11-08 23:19:08 +0800

    Add expenditure test

--- src/test/java/seedu/address/model/expenditure/ExpenditureTest.java
+++ src/test/java/seedu/address/model/expenditure/ExpenditureTest.java
@@ -1,121 +1,101 @@
-//package seedu.address.model.expenditure;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static seedu.address.testutil.TypicalExpenditure.MC_MUFFIN;
-//
-//import java.time.LocalDate;
-//import java.util.HashSet;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import seedu.address.testutil.ExpenditureBuilder;
-//
-//
-//
-//
-//class ExpenditureTest {
-//
-//    @Test
-//    public void isSameExpenditure() {
-//        // same object -> returns true
-//        assertTrue(MC_MUFFIN.isSameExpenditure(MC_MUFFIN));
-//
-//        // null -> returns false
-//        assertFalse(MC_MUFFIN.isSameExpenditure(null));
-//
-//        // different createdOn -> returns false
-//        Expenditure editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN).withCreatedOn("2020-10-13").build();
-//        assertFalse(MC_MUFFIN.isSameExpenditure(editedMcmuffin));
-//
-//        // different name -> returns false
-//        editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN).withName("McSpicy").build();
-//        assertFalse(MC_MUFFIN.isSameExpenditure(editedMcmuffin));
-//
-//        // different price -> returns false
-//        editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN).withPrice("12.00").build();
-//        assertFalse(MC_MUFFIN.isSameExpenditure(editedMcmuffin));
-//
-//        // same name, same createdOn, different tags -> returns true
-//        editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN).withTags("Burger").build();
-//        assertTrue(MC_MUFFIN.isSameExpenditure(editedMcmuffin));
-//    }
-//
-//    @Test
-//    public void equals() {
-//        // same values -> returns true
-//        Expenditure mcmuffinCopy = new ExpenditureBuilder(MC_MUFFIN).build();
-//        assertTrue(MC_MUFFIN.equals(mcmuffinCopy));
-//
-//        // same object -> returns true
-//        assertTrue(MC_MUFFIN.equals(MC_MUFFIN));
-//
-//        // null -> returns false
-//        assertFalse(MC_MUFFIN.equals(null));
-//
-//        // different type -> returns false
-//        assertFalse(MC_MUFFIN.equals(5));
-//
-//        // different budget -> returns false
-//        assertFalse(MC_MUFFIN.equals(new ExpenditureBuilder().build()));
-//
-//        // different name -> returns false
-//        Expenditure editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN)
-//                .withName(ExpenditureBuilder.DEFAULT_NAME).build();
-//        assertFalse(MC_MUFFIN.equals(editedMcmuffin));
-//
-//        // different createdOn -> returns false
-//        editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN)
-//                .withCreatedOn(ExpenditureBuilder.DEFAULT_CREATED_ON).build();
-//        assertFalse(MC_MUFFIN.equals(editedMcmuffin));
-//
-//        // different price -> returns false
-//        editedMcmuffin = new ExpenditureBuilder(MC_MUFFIN)
-//                .withPrice(ExpenditureBuilder.DEFAULT_PRICE).build();
-//        assertFalse(MC_MUFFIN.equals(editedMcmuffin));
-//    }
-//    @Test
-//    void getName() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertEquals(expectedExpenditure.getName(), new Name("McMuffin"));
-//    }
-//
-//    @Test
-//    void getPrice() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertEquals(expectedExpenditure.getPrice(), new Price("4.50"));
-//    }
-//
-//    @Test
-//    void getCreatedOn() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertEquals(expectedExpenditure.getCreatedOn(), new Date("2020-10-10"));
-//    }
-//
-//    @Test
-//    void getFormattedCreatedOn() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertEquals(expectedExpenditure.getFormattedCreatedOn(),
-//                java.sql.Date.valueOf(LocalDate.parse("2020-10-10")));
-//    }
-//
-//    @Test
-//    void getTags() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertEquals(expectedExpenditure.getTags(), new HashSet<>());
-//    }
-//
-//
-//    @Test
-//    void testToString() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertEquals(expectedExpenditure.toString(), "McMuffin");
-//    }
-//
-//    @Test
-//    void contains() {
-//        Expenditure expectedExpenditure = MC_MUFFIN;
-//        assertTrue(expectedExpenditure.contains("MC"));
-//    }
-//}
+package seedu.address.model.expenditure;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.testutil.ExpenditureBuilder;
+import seedu.address.testutil.TypicalBudgets;
+import seedu.address.testutil.TypicalExpenditure;
+
+
+class ExpenditureTest {
+
+    private Expenditure mcMuffin;
+
+    @BeforeEach
+    public void setUp() {
+        mcMuffin = TypicalExpenditure.getMcMuffinExpenditure();
+    }
+
+    @Test
+    public void equalsTest() {
+        // same object -> returns true
+        assertTrue(mcMuffin.equals(mcMuffin));
+
+        // different type -> returns false
+        assertFalse(mcMuffin.equals(5));
+
+        // null -> returns false
+        assertFalse(mcMuffin.equals(null));
+
+        // different expenditure -> returns false
+        assertFalse(mcMuffin.equals(new ExpenditureBuilder(TypicalExpenditure.getKfcBanditoExpenditure()).build()));
+
+        // different createdOn -> returns false
+        Expenditure editedMcmuffin = new ExpenditureBuilder(mcMuffin).withCreatedOn("2020-10-13").build();
+        assertFalse(mcMuffin.equals(editedMcmuffin));
+
+        // different name -> returns false
+        editedMcmuffin = new ExpenditureBuilder(mcMuffin).withName("McSpicy").build();
+        assertFalse(mcMuffin.equals(editedMcmuffin));
+
+        // different price -> returns false
+        editedMcmuffin = new ExpenditureBuilder(mcMuffin).withPrice("12.00").build();
+        assertFalse(mcMuffin.equals(editedMcmuffin));
+
+        // different tags -> returns false
+        editedMcmuffin = new ExpenditureBuilder(mcMuffin).withTags("Burger").build();
+        assertFalse(mcMuffin.equals(editedMcmuffin));
+
+        // same value -> return true
+        editedMcmuffin = new ExpenditureBuilder(mcMuffin).build();
+        assertTrue(mcMuffin.equals(editedMcmuffin));
+    }
+
+    @Test
+    void getName() {
+        assertEquals(new Name("McMuffin"), mcMuffin.getName());
+    }
+
+    @Test
+    void getPrice() {
+        assertEquals(new Price("4.50"), mcMuffin.getPrice());
+    }
+
+    @Test
+    void getCreatedOn() {
+        assertEquals(new Date("2020-10-10"), mcMuffin.getCreatedOn());
+    }
+
+    @Test
+    void getFormattedCreatedOn() {
+        assertEquals(java.sql.Date.valueOf(LocalDate.parse("2020-10-10")),
+                mcMuffin.getFormattedCreatedOn());
+    }
+
+    @Test
+    void getTags() {
+        assertEquals(new HashSet<>(), mcMuffin.getTags());
+    }
+
+
+    @Test
+    void testToString() {
+        assertEquals("McMuffin", mcMuffin.toString());
+    }
+
+    @Test
+    void contains() {
+        assertTrue(mcMuffin.contains("MC"));
+    }
+}
